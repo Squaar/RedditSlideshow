96026795e6fed73bf6e13f7c4725ff5d70377dcc
Matt Dumford <mdumford99@gmail.com>
2014-10-30 00:15:10 -0500

    re-organized everthing

--- redditslideshow.py
+++ redditslideshow.py
@@ -2,95 +2,103 @@ import praw
 import argparse
 from urllib.request import urlopen
 
-#grabs submissions from subreddits and returns list of urls
-def getSubmissions(sub, number, list):
-	urls = []
-	for sub in subs:
-		sub["submissions"] = []
-		if list == "hot":
-			for submission in sub["sub"].get_hot(limit=args.number):
-				sub["submissions"].append(submission)
-				if submission.url is not None:
-					urls.append(submission.url)
+class RedditSlideshow:
 
-		elif list == "new":
-			for submission in sub["sub"].get_new(limit=args.number):
-				sub["submissions"].append(submission)
-				if submission.url is not None:
-					urls.append(submission.url)
+	def __init__(self):
+		parser = argparse.ArgumentParser()
+		parser.add_argument("file", help="The file containing a list of subreddits, one per line, to read in from.")
+		parser.add_argument("number", type=int, default=20, nargs="?", help="The number of submissions to pull from each subreddit")
+		parser.add_argument("list", default="hot", nargs="?", help="hot, new, rising, controversial, or top.")
+		self.args = parser.parse_args()
 
-		elif list == "rising":
-			for submission in sub["sub"].get_rising(limit=args.number):
-				sub["submissions"].append(submission)
-				if submission.url is not None:
-					urls.append(submission.url)
+		#check if list is valid
+		validLists = ["hot", "new", "rising", "controversial", "top"]
+		self.args.list = self.args.list.lower()
+		if self.args.list not in validLists:
+			print("List is not valid. redditslideshow.py -h for help.")
+			exit()
 
-		elif list == "controversial":
-			for submission in sub["sub"].get_controversial(limit=args.number):
-				sub["submissions"].append(submission)
-				if submission.url is not None:
-					urls.append(submission.url)
+		self.reddit = praw.Reddit(user_agent="RedditGrabber v0.1")
+		self.subs = self.get_sub_names()
+		self.urls = self.get_submission_urls()
+		self.clean_urls()
 
-		elif list == "top":
-			for submission in sub["sub"].get_top(limit=args.number):
-				sub["submissions"].append(submission)
-				if submission.url is not None:
-					urls.append(submission.url)
-	return urls
-
-#tries to remove non images, and add file extensions to dumb imgur posts
-def cleanURLs(urls):
-	for url in urls:
-		if "imgur.com" in url and url[-4] != ".":
-			urls.remove(url)
-			url = url + ".png"
-			urls.append(url)
-		elif ".htm" in url or url[-1] == "/" or "reddit.com" in url or url[-4] != ".":
-			urls.remove(url)
-			continue
-		
+	def get_sub_names(self):
+		subs = []
+		with open(self.args.file, 'r') as subsFile:
+			for line in subsFile:
+				line = line.strip()
+				if len(line) > 0:
+					subs.append({"name": line, "sub": self.reddit.get_subreddit(line)})
+		return subs
 
 
+	def run(self):
+		for i, url in enumerate(self.urls):
+			print(url + '... ', end='')
+			with open(url.split('/')[-1].split('?')[0], 'wb') as f:
+				try:
+					f.write(urlopen(self.urls[i]).read())
+					print('SUCCESS')
+				except Exception as e:
+					print('FAIL')
+					print(e)
+		self.cleanup()
 
-parser = argparse.ArgumentParser()
-parser.add_argument("file", help="The file containing a list of subreddits, one per line, to read in from.")
-parser.add_argument("number", type=int, default=20, nargs="?", help="The number of submissions to pull from each subreddit")
-parser.add_argument("list", default="hot", nargs="?", help="hot, new, rising, controversial, or top.")
-args = parser.parse_args()
+	def cleanup(self):
+		pass
 
-#check if list is valid
-validLists = ["hot", "new", "rising", "controversial", "top"]
-args.list = args.list.lower()
-if args.list not in validLists:
-	print("List is not valid. redditslideshow.py -h for help.")
-	exit()
+	#grabs submissions from subreddits and returns list of urls
+	def get_submission_urls(self):
+		urls = []
+		for sub in self.subs:
+			sub["submissions"] = []
+			if self.args.list == "hot":
+				for submission in sub["sub"].get_hot(limit=self.args.number):
+					sub["submissions"].append(submission)
+					if submission.url is not None:
+						urls.append(submission.url)
 
-reddit = praw.Reddit(user_agent="RedditGrabber v0.1")
+			elif self.args.list == "new":
+				for submission in sub["sub"].get_new(limit=self.args.number):
+					sub["submissions"].append(submission)
+					if submission.url is not None:
+						urls.append(submission.url)
 
-#set up list of subreddits
-subsFile = open(args.file, "r")
-subs = []
-for line in subsFile:
-	line = line.strip()
-	if len(line) > 0:
-		subs.append({"name": line, "sub": reddit.get_subreddit(line)})
-subsFile.close()
+			elif self.args.list == "rising":
+				for submission in sub["sub"].get_rising(limit=self.args.number):
+					sub["submissions"].append(submission)
+					if submission.url is not None:
+						urls.append(submission.url)
 
-urls = getSubmissions(subs, args.number, args.list)
-cleanURLs(urls)
+			elif self.args.list == "controversial":
+				for submission in sub["sub"].get_controversial(limit=self.args.number):
+					sub["submissions"].append(submission)
+					if submission.url is not None:
+						urls.append(submission.url)
 
-# for url in urls:
-# 	print(url)
+			elif self.args.list == "top":
+				for submission in sub["sub"].get_top(limit=self.args.number):
+					sub["submissions"].append(submission)
+					if submission.url is not None:
+						urls.append(submission.url)
+		return urls
 
-for i in range(0,len(urls)):
-	f = None
-	try:
-		f = open(str(i) + ".png", "wb")
-		f.write(urlopen(urls[i]).read())
-		f.close()
-		print(str(i) + ".png downloaded!")
-	except:
-		f.close()
-		print(str(i) + ".png failed!")
+	#tries to remove non images, and add file extensions to dumb imgur posts
+	def clean_urls(self):
+		for url in self.urls:
+			if "imgur.com" in url and url[-4] != ".":
+				self.urls.remove(url)
+				url = url + ".png"
+				self.urls.append(url)
+			elif ".htm" in url or url[-1] == "/" or "reddit.com" in url or url[-4] != ".":
+				self.urls.remove(url)
+				continue
 
-print("done.")
+if __name__ == '__main__':
+	RedditSlideshow().run()
+
+
+
+
+
